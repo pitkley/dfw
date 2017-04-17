e0ba1e7a0cf2843fdad725128c68da2d8b034814
Pit Kleyersburg <pitkley@googlemail.com>
2017-04-17 13:59:23 +0200

    Add rule builder, fix container to container logic

--- src/dfwrs.rs
+++ src/dfwrs.rs
@@ -5,6 +5,7 @@ use std::collections::HashMap as Map;
 use boondock::{ContainerListOptions, Docker};
 use boondock::container::Container;
 use boondock::container::Network as ContainerNetwork;
+use boondock::network::Network;
 use iptables::IPTables;
 
 use docker::*;
@@ -12,6 +13,85 @@ use errors::*;
 use types::*;
 
 const DFWRS_FORWARD_CHAIN: &'static str = "DFWRS_FORWARD";
+
+#[derive(Debug, Clone, Default)]
+struct Rule {
+    pub source: Option<String>,
+    pub destination: Option<String>,
+    pub in_interface: Option<String>,
+    pub out_interface: Option<String>,
+    pub protocol: Option<String>,
+    pub jump: String,
+}
+
+#[allow(dead_code)]
+impl Rule {
+    pub fn source(&mut self, value: String) -> &mut Self {
+        let mut new = self;
+        new.source = Some(value);
+        new
+    }
+
+    pub fn destination(&mut self, value: String) -> &mut Self {
+        let mut new = self;
+        new.destination = Some(value);
+        new
+    }
+
+    pub fn in_interface(&mut self, value: String) -> &mut Self {
+        let mut new = self;
+        new.in_interface = Some(value);
+        new
+    }
+
+    pub fn out_interface(&mut self, value: String) -> &mut Self {
+        let mut new = self;
+        new.out_interface = Some(value);
+        new
+    }
+
+    pub fn protocol(&mut self, value: String) -> &mut Self {
+        let mut new = self;
+        new.protocol = Some(value);
+        new
+    }
+
+    pub fn jump(&mut self, value: String) -> &mut Self {
+        let mut new = self;
+        new.jump = value;
+        new
+    }
+
+    pub fn build(&self) -> Result<String> {
+        let mut args: Vec<String> = Vec::new();
+
+        if let Some(ref source) = self.source {
+            args.push("-s".to_owned());
+            args.push(source.to_owned());
+        }
+        if let Some(ref destination) = self.destination {
+            args.push("-d".to_owned());
+            args.push(destination.to_owned());
+        }
+        if let Some(ref in_interface) = self.in_interface {
+            args.push("-i".to_owned());
+            args.push(in_interface.to_owned());
+        }
+        if let Some(ref out_interface) = self.out_interface {
+            args.push("-o".to_owned());
+            args.push(out_interface.to_owned());
+        }
+        if let Some(ref protocol) = self.protocol {
+            args.push("-p".to_owned());
+            args.push(protocol.to_owned());
+        }
+
+        args.push("-j".to_owned());
+        args.push(self.jump.to_owned());
+
+        Ok(args.join(" "))
+    }
+}
 
 pub fn process(docker: &Docker, dfw: &DFW, ipt4: &IPTables, ipt6: &IPTables) -> Result<()> {
     // TODO: external_network_interface
@@ -70,11 +150,14 @@ fn process_container_to_container(docker: &Docker,
 
     let containers = docker.containers(ContainerListOptions::default().all())?;
     let container_map = get_container_map(&containers)?;
+    let networks = docker.networks()?;
+    let network_map = get_network_map(&networks)?;
 
     if ctc.rules.is_some() && container_map.is_some() {
         process_ctc_rules(docker,
                           &ctc.rules.as_ref().unwrap(),
                           container_map.unwrap(),
+                          network_map.unwrap(),
                           ipt4,
                           ipt6)?;
     }
@@ -88,11 +171,11 @@ fn process_container_to_container(docker: &Docker,
     Ok(())
 }
 
-fn get_network(container_name: &String,
-               network_name: &String,
-               docker: &Docker,
-               container_map: &Map<String, &Container>)
-               -> Result<Option<ContainerNetwork>> {
+fn get_network_for_container(container_name: &String,
+                             network_name: &String,
+                             docker: &Docker,
+                             container_map: &Map<String, &Container>)
+                             -> Result<Option<ContainerNetwork>> {
     // Check if `container_name` exists
     if !container_map.contains_key(container_name) {
         return Ok(None);
@@ -111,59 +194,56 @@ fn get_network(container_name: &String,
 fn process_ctc_rules(docker: &Docker,
                      rules: &Vec<ContainerToContainerRule>,
                      container_map: Map<String, &Container>,
+                     network_map: Map<String, &Network>,
                      ipt4: &IPTables,
                      _ipt6: &IPTables)
                      -> Result<()> {
     for rule in rules {
         println!("{:#?}", rule);
-        let mut args: Vec<String> = Vec::new();
-        let mut src_network_device_name: Option<String> = None;
+        let mut ipt_rule = Rule::default();
 
-        // TODO: refactor into `get_network` (issue #14)
-        // Push arguments in regards to the source network
+        let network = match network_map.get(&rule.network) {
+            Some(network) => network,
+            None => continue,
+        };
+        let bridge_name = format!("br-{}", &network.Id[..12]);
+        ipt_rule
+            .in_interface(bridge_name.to_owned())
+            .out_interface(bridge_name.to_owned());
+
         if let Some(ref src_container) = rule.src_container {
-            let src_network =
-                match get_network(src_container, &rule.network, docker, &container_map)? {
-                    Some(network) => network,
-                    None => continue,
-                };
+            let src_network = match get_network_for_container(src_container,
+                                                              &rule.network,
+                                                              docker,
+                                                              &container_map)? {
+                Some(network) => network,
+                None => continue,
+            };
 
-            // Create the rule
-            args.push("-i".to_owned());
             let bridge_name = format!("br-{}", &src_network.NetworkID[..12]);
-            args.push(bridge_name.clone());
-
-            args.push("-s".to_owned());
-            args.push(src_network.IPAddress.clone());
-
-            src_network_device_name = Some(bridge_name.clone())
+            ipt_rule
+                .in_interface(bridge_name.to_owned())
+                .out_interface(bridge_name.to_owned())
+                .source(src_network.IPAddress.to_owned());
         }
 
-        // Push arguments in regards to the destination network
         if let Some(ref dst_container) = rule.dst_container {
-            let dst_network =
-                match get_network(dst_container, &rule.network, docker, &container_map)? {
-                    Some(network) => network,
-                    None => continue,
-                };
+            let dst_network = match get_network_for_container(dst_container,
+                                                              &rule.network,
+                                                              docker,
+                                                              &container_map)? {
+                Some(network) => network,
+                None => continue,
+            };
 
-            // Create the rule
-            args.push("-o".to_owned());
-            args.push(format!("br-{}", &dst_network.NetworkID[..12]));
-
-            args.push("-d".to_owned());
-            args.push(dst_network.IPAddress.clone());
-        } else {
-            // If there is no destination network but there was a source network, push that again
-            if let Some(device_name) = src_network_device_name {
-                args.push("-o".to_owned());
-                args.push(device_name);
-            }
+            let bridge_name = format!("br-{}", &dst_network.NetworkID[..12]);
+            ipt_rule
+                .out_interface(bridge_name.to_owned())
+                .destination(dst_network.IPAddress.to_owned());
         }
 
-        // Push the action
-        args.push("-j".to_owned());
-        args.push(rule.action.clone());
+        // Set jump
+        ipt_rule.jump(rule.action.to_owned());
 
         // Apply the rule
         ipt4.append("filter", DFWRS_FORWARD_CHAIN, &rule_str)?;
