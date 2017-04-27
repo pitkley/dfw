a1ac30cf50f0371987f4d7191ada33b787b7ddff
Pit Kleyersburg <pitkley@googlemail.com>
2017-04-27 17:47:38 +0200

    Add documentation

--- README.md
+++ README.md
@@ -1,18 +1,124 @@
-# DFWRS
+# DFWRS - Docker Firewall Framework in Rust
 
-## License
+`dfwrs` is conceptually based on the [Docker Firewall Framework, `dfwfw`][dfwfw-github]. Its
+goal is to make firewall administration with Docker simpler, but also more extensive by trying
+to replace the Docker built-in firewall handling by direct interaction with iptables.
 
-DFWRS is licensed under either of
+This is accomplished by a flexible configuration which defines how the firewall should be built
+up. While DFWRS is running, Docker container events will be monitored and the rules rebuilt
+when necessary.
 
-* Apache License, Version 2.o, ([LICENSE-APACHE](LICENSE-APACHE) or
-  http://www.apache.org/licenses/LICENSE-2.0)
-* MIT license ([LICENSE-MIT](LICENSE-MIT) or
-  http://opensource.org/licenses/MIT)
+See [DFWFW's README][dfwfw-readme] for more insight. Most of what you will read there will be
+applicable to DFWRS.
 
-at your option.
+## Configuration
 
-### Contribution
+The general configuration happens across six categories:
 
-Unless you explicitly state otherwise, any contribution intentionally submitted
-for inclusion in DFWRS by you, as defined in the Apache-2.0 license, shall be
-dual licensed as above, without any additional terms or conditions.
+* `defaults`
+
+    This category defines global, default values to be used by DFWRS and the other categories.
+
+* `container_to_container`
+
+    This controls the communication between containers and across [Docker
+    networks][docker-networks].
+
+* `container_to_wider_world`
+
+    This controls if and how containers may access the wider world, i.e. what they can
+    communicate across the `OUTPUT` chain on the host.
+
+* `container_to_host`
+
+    To restrict or allow access to the host, this section is used.
+
+* `wider_world_to_container`
+
+    This controls how the wider world, i.e. whatever comes in through the `INPUT` chain on the
+    host, can communicate with a container or a Docker network.
+
+* `container_dnat`
+
+    This category allows you to define specific rules for destination network address
+    translation, even or especially across Docker networks.
+
+One category which DFWFW covers that is not (yet) implemented in DFWRS is
+`container_internals`, that is configuring iptables rules within containers.
+
+See the [examples][examples] *(TODO)*, and the [configuration types][types.rs] for a detailed
+description of every configuration section.
+
+## Supported Docker versions
+
+At least Docker 1.9.0 is required, since we heavily rely on the Docker [networking
+feature][docker-networks] which was introduced in 1.9.0.
+
+DFWRS has been successfully tested under the following Docker versions:
+
+* `17.03.1-ce`
+
+* `17.04.0-ce`
+
+It is planned to introduce some form of automated testing to cover as many Docker versions as
+possible.
+
+## Installation
+
+While you can use Cargo to install `dfwrs` as a binary, using the Docker image is the preferred
+way to go, especially if you don't want to install Rust and Cargo on your host:
+
+```console
+$ docker pull pitkley/dfwrs:0.2
+$ docker run -d \
+      --name=dfwrs \
+      -v /var/run/docker.sock:/var/run/docker.sock:ro \
+      -v /path/to/your/config:/config \
+      --net host --cap-add=NET_ADMIN \
+      pitkley/dfwrs --config-path /config
+```
+
+This will download a lightweight image, coming in at under 6 MB, and subsequently run it using
+your configuration.
+
+## Motivation for this reimplementation
+
+I have reimplemented DFWFW in Rust for two reasons:
+
+1. DFWFW has lost compatibility with the Docker API starting with release 17.04.0-ce.
+
+    This is very likely due to a change in Dockers web API regarding getting networks and their
+    containers, see [this relevant issue][moby-issue-32686]. Now, it would almost certainly have
+    been easier to fix this issue in DFWFW -- if not for me, maybe for the maintainer. I have
+    [created an issue][dfwfw-issue-13] to give the DFWFW maintainer a heads-up.
+
+2. But the main reason for this reimplementation was that I found a real-life project to tackle
+   with Rust. This project allowed me to delve into quite a few different aspects and facets of
+   Rust and especially its eco-system, amongst others:
+
+  * [`clap`][crates-clap], for parsing of command line arguments
+  * [`chan`][crates-chan], for easy messaging and coordination between threads
+  * [`error_chain`][crates-error_chain], for simplified application wide error handling
+  * [Serde][crates-serde], for deserialization of the TOML configuration
+  * [`slog`][crates-slog], for structured logging
+
+    Disregarding the obvious hair-pulling moments regarding ownership, borrowing and lifetimes,
+    my experience with Rust, and its brillant eco-system has been an absolute pleasure.
+
+
+[crates-clap]: https://crates.io/crates/clap
+[crates-chan]: https://crates.io/crates/chan
+[crates-error_chain]: https://crates.io/crates/error-chain
+[crates-serde]: https://crates.io/crates/serde
+[crates-slog]: https://crates.io/crates/slog
+
+[dfwfw-github]: https://github.com/irsl/dfwfw
+[dfwfw-issue-13]: https://github.com/irsl/dfwfw/issues/13
+[dfwfw-readme]: https://github.com/irsl/dfwfw/blob/master/README.md
+
+[docker-networks]: https://docs.docker.com/engine/userguide/networking/
+
+[moby-issue-32686]: https://github.com/moby/moby/issues/32686
+
+[types.rs]: types/index.html
+>>>>>>> Add documentation
--- src/types.rs
+++ src/types.rs
@@ -6,6 +6,58 @@
 // option. This file may not be copied, modified or distributed
 // except according to those terms.
 
+//! # Types
+//!
+//! The types in this module make up the structure of the configuration-file(s).
+//!
+//! ## Example
+//!
+//! The following is an examplary TOML configuration, which will be parsed into this modules types.
+//!
+//! ```toml
+//! [defaults]
+//! external_network_interfaces = "eth0"
+//!
+//! [initialization]
+//! [initialization.v4]
+//! filter = [
+//!     "-P INPUT DROP",
+//! ]
+//!
+//! [container_to_container]
+//! default_policy = "DROP"
+//!
+//! [[container_to_container.rules]]
+//! network = "common_network"
+//! src_container = "container_a"
+//! dst_container = "container_b"
+//! action = "ACCEPT"
+//!
+//! [container_to_wider_world]
+//! default_policy = "ACCEPT"
+//!
+//! [[container_to_container.rules]]
+//! network = "other_network"
+//! src_container = "container_c"
+//! action = "DROP"
+//!
+//! [wider_world_to_container]
+//!
+//! [[wider_world_to_container.rules]]
+//! network = "common_network"
+//! dst_container = "container_a"
+//! expose_port = [80, 443]
+//!
+//! [container_dnat]
+//!
+//! [[container_dnat.rules]]
+//! src_network = "common_network"
+//! src_container = "container_a"
+//! dst_network = "other_network"
+//! dst_container = "container_c"
+//! expose_port = { host_port = 8080, container_port = 80, family = "tcp" }
+//! ```
+
 use std::collections::HashMap as Map;
 use std::fmt;
 use std::marker::PhantomData;
@@ -15,105 +67,313 @@ use serde::de::{self, Deserialize, Deserializer, DeserializeSeed};
 
 const DEFAULT_PROTOCOL: &'static str = "tcp";
 
+/// `DFW` is the parent type defining the complete configuration used by DFWRS to build up the
+/// firewall rules.
+///
+/// Every section is optional.
 #[derive(Deserialize, Debug, Clone)]
 #[serde(deny_unknown_fields)]
 pub struct DFW {
+    /// The `defaults` configuration section
     pub defaults: Option<Defaults>,
+    /// The `initialization` configuration section
     pub initialization: Option<Initialization>,
+    /// The `container_to_container` configuration section
     pub container_to_container: Option<ContainerToContainer>,
+    /// The `container_to_wider_world` configuration section
     pub container_to_wider_world: Option<ContainerToWiderWorld>,
+    /// The `container_to_host` configuration section
     pub container_to_host: Option<ContainerToHost>,
+    /// The `wider_world_to_container` configuration section
     pub wider_world_to_container: Option<WiderWorldToContainer>,
+    /// The `container_dnat` configuration section
     pub container_dnat: Option<ContainerDNAT>,
 }
 
+/// The default configuration section, used by DFWRS for rule processing.
 #[derive(Deserialize, Debug, Clone)]
 #[serde(deny_unknown_fields)]
 pub struct Defaults {
+    /// This defines the external network interfaces of the host to consider during building the
+    /// rules. The value can be non-existant, a string, or a sequence of strings.
+    ///
+    /// # Example
+    ///
+    /// ```toml
+    /// external_network_interfaces = "eth0"
+    /// external_network_interfaces = ["eth0", "eth1"]
+    /// ```
     #[serde(default, deserialize_with = "option_string_or_seq_string")]
     pub external_network_interfaces: Option<Vec<String>>,
 }
 
+/// The initialization section allows you to add custom rules to any table in both iptables and
+/// ip6tables.
+///
+/// # Example
+///
+/// ```toml
+/// [initialization.v4]
+/// filter = [
+///     "-P INPUT DROP",
+///     "-F INPUT",
+///     # ...
+/// ]
+///
+/// [initialization.v6]
+/// nat = [
+///     "-P PREROUTING DROP",
+///     # ...
+/// ]
+/// ```
 #[derive(Deserialize, Debug, Clone)]
 #[serde(deny_unknown_fields)]
 pub struct Initialization {
+    /// Initialization rules for iptables (IPv4). Expects a map where the key is a specific table
+    /// and the value is a list of rules.
+    ///
+    /// # Example
+    ///
+    /// ```toml
+    /// [initialization.v4]
+    /// filter = [
+    ///     "-P INPUT DROP",
+    ///     "-F INPUT",
+    ///     # ...
+    /// ]
+    /// ```
     pub v4: Option<Map<String, Vec<String>>>,
+
+    /// Initialization rules for ip6tables (IPv6). Expects a map where the key is a specific table
+    /// and the value is a list of rules.
+    ///
+    /// # Example
+    ///
+    /// ```toml
+    /// [initialization.v6]
+    /// nat = [
+    ///     "-P PREROUTING DROP",
+    ///     # ...
+    /// ]
+    /// ```
     pub v6: Option<Map<String, Vec<String>>>,
 }
 
+/// The container-to-container section, defining how containers can communicate amongst each other.
 #[derive(Deserialize, Debug, Clone)]
 #[serde(deny_unknown_fields)]
 pub struct ContainerToContainer {
+    /// The `default_policy` defines the default for when there is not a specific rule.
     pub default_policy: String,
+    /// An optional list of rules, see
+    /// [`ContainerToContainerRule`](struct.ContainerToContainerRule.html).
+    ///
+    /// # Example
+    ///
+    /// The easiest way to define the rules is using TOMLs [arrays of tables][toml-aot]:
+    ///
+    /// ```toml
+    /// [[container_to_container.rules]]
+    /// # first rule here
+    /// [[container_to_container.rules]]
+    /// # second rule here
+    /// ```
+    ///
+    /// [toml-aot]:
+    ///  https://github.com/toml-lang/toml/blob/master/versions/en/toml-v0.4.0.md#array-of-tables
     pub rules: Option<Vec<ContainerToContainerRule>>,
 }
 
+/// Definition for a rule to be used in the container-to-container section.
 #[derive(Deserialize, Debug, Clone)]
 #[serde(deny_unknown_fields)]
 pub struct ContainerToContainerRule {
+    /// Common network between the source container and the destination container to apply the rule
+    /// to.
     pub network: String,
+    /// Source container to apply the rule to.
     pub src_container: Option<String>,
+    /// Destination container to apply the rule to.
     pub dst_container: Option<String>,
+    /// Additional filter, which will be added to the iptables command.
     pub filter: Option<String>,
+    /// Action to take (i.e. `ACCEPT`, `DROP`, `REFUSE`).
     pub action: String,
 }
 
+/// The container-to-wider-world section, defining how containers can communicate with the wider
+/// world.
 #[derive(Deserialize, Debug, Clone)]
 #[serde(deny_unknown_fields)]
 pub struct ContainerToWiderWorld {
+    /// The `default_policy` defines the default for when there is not a specific rule.
     pub default_policy: String,
+    /// An optional list of rules, see
+    /// [`ContainerToWiderWorldRule`](struct.ContainerToWiderWorldRule.html).
+    ///
+    /// # Example
+    ///
+    /// The easiest way to define the rules is using TOMLs [arrays of tables][toml-aot]:
+    ///
+    /// ```toml
+    /// [[container_to_wider_world.rules]]
+    /// # first rule here
+    /// [[container_to_wider_world.rules]]
+    /// # second rule here
+    /// ```
+    ///
+    /// [toml-aot]:
+    ///  https://github.com/toml-lang/toml/blob/master/versions/en/toml-v0.4.0.md#array-of-tables
     pub rules: Option<Vec<ContainerToWiderWorldRule>>,
 }
 
+/// Definition for a rule to be used in the container-to-wider-world section.
 #[derive(Deserialize, Debug, Clone)]
 #[serde(deny_unknown_fields)]
 pub struct ContainerToWiderWorldRule {
+    /// Network of the source container to apply the rule to.
     pub network: Option<String>,
+    /// Source container to apply the rule to.
     pub src_container: Option<String>,
+    /// Additional filter, which will be added to the iptables command.
     pub filter: Option<String>,
+    /// Action to take (i.e. `ACCEPT`, `DROP`, `REFUSE`).
     pub action: String,
+    /// Specific external network interface to target.
     pub external_network_interface: Option<String>,
 }
 
+/// The container-to-host section, defining how containers can communicate with the host.
 #[derive(Deserialize, Debug, Clone)]
 #[serde(deny_unknown_fields)]
 pub struct ContainerToHost {
+    /// The `default_policy` defines the default for when there is not a specific rule.
     pub default_policy: String,
+    /// An optional list of rules, see
+    /// [`ContainerToHostRule`](struct.ContainerToHostRule.html).
+    ///
+    /// # Example
+    ///
+    /// The easiest way to define the rules is using TOMLs [arrays of tables][toml-aot]:
+    ///
+    /// ```toml
+    /// [[container_to_host.rules]]
+    /// # first rule here
+    /// [[container_to_host.rules]]
+    /// # second rule here
+    /// ```
+    ///
+    /// [toml-aot]:
+    ///  https://github.com/toml-lang/toml/blob/master/versions/en/toml-v0.4.0.md#array-of-tables
     pub rules: Option<Vec<ContainerToHostRule>>,
 }
 
+/// Definition for a rule to be used in the container-to-host section.
 #[derive(Deserialize, Debug, Clone)]
 #[serde(deny_unknown_fields)]
 pub struct ContainerToHostRule {
+    /// Network of the source container to apply the rule to.
     pub network: String,
+    /// Source container to apply the rule to.
     pub src_container: Option<String>,
+    /// Additional filter, which will be added to the iptables command.
     pub filter: Option<String>,
+    /// Action to take (i.e. `ACCEPT`, `DROP`, `REFUSE`).
     pub action: String,
 }
 
+/// The wider-world-to-container section, defining how containers can reached from the wider world.
 #[derive(Deserialize, Debug, Clone)]
 #[serde(deny_unknown_fields)]
 pub struct WiderWorldToContainer {
+    /// An optional list of rules, see
+    /// [`WiderWorldToContainerRule`](struct.WiderWorldToContainerRule.html).
+    ///
+    /// # Example
+    ///
+    /// The easiest way to define the rules is using TOMLs [arrays of tables][toml-aot]:
+    ///
+    /// ```toml
+    /// [[wider_world_to_container.rules]]
+    /// # first rule here
+    /// [[wider_world_to_container.rules]]
+    /// # second rule here
+    /// ```
+    ///
+    /// [toml-aot]:
+    ///  https://github.com/toml-lang/toml/blob/master/versions/en/toml-v0.4.0.md#array-of-tables
     pub rules: Option<Vec<WiderWorldToContainerRule>>,
 }
 
+/// Definition for a rule to be used in the wider-world-to-container section.
 #[derive(Deserialize, Debug, Clone)]
 #[serde(deny_unknown_fields)]
 pub struct WiderWorldToContainerRule {
+    /// Network of the destination container to apply the rule to.
     pub network: String,
+
+    /// Destination container to apply the rule to.
     pub dst_container: String,
+
+    /// Ports to apply the rule to.
+    ///
+    /// Defined as:
+    ///
+    /// * a single integer
+    ///
+    /// * a single string
+    ///
+    /// * a single struct
+    ///
+    /// * a list of integers
+    ///
+    /// * a list of strings
+    ///
+    /// * a list of structs
+    ///
+    /// # Example
+    ///
+    /// All of the following are legal TOML fragments:
+    ///
+    /// ```toml
+    /// expose_port = 80
+    /// expose_port = [80, 443]
+    /// expose_port = "53/udp"
+    /// expose_port = ["80/tcp", "53/udp"]
+    ///
+    /// # The following four all result in the same definition
+    /// expose_port = { host_port = 8080 }
+    /// expose_port = { host_port = 8080, container_port = 8080 }
+    /// expose_port = { host_port = 8080, family = "tcp" }
+    /// expose_port = { host_port = 8080, container_port = 8080, family = "tcp" }
+    ///
+    /// expose_port = [
+    ///     { host_port = 80 },
+    ///     { host_port = 53, family = "udp" },
+    ///     { host_port = 443, container_port = 8443 },
+    /// ]
+    /// ```
     #[serde(deserialize_with = "single_or_seq_string_or_struct")]
     pub expose_port: Vec<ExposePort>,
+
+    /// Specific external network interface to target.
     pub external_network_interface: Option<String>,
 }
 
+/// Struct to hold a port definition to expose on the host/between containers.
 #[derive(Deserialize, Debug, Clone, Default, Builder)]
 #[serde(deny_unknown_fields)]
 pub struct ExposePort {
+    /// Port the `container_port` should be exposed to on the host.
     pub host_port: u16,
+
+    /// Port the `host_port` should map to into the container.
     #[builder(default="self.default_container_port()?")]
     pub container_port: Option<u16>,
 
+    /// Family of the exposed port.
+    ///
+    /// Can be left blank, `tcp` will be used as default.
     #[serde(default = "default_expose_port_family")]
     #[builder(default = "self.default_family()?")]
     pub family: String,
@@ -132,6 +392,19 @@ impl ExposePortBuilder {
 impl FromStr for ExposePort {
     type Err = String;
 
+    /// Convert a formatted string into a [`ExposePort`](struct.ExposePort.html).
+    ///
+    /// The string has to be in the format `<HOST_PORT>/<FAMILY>`, i.e. `80/tcp`. Specifying the
+    /// container-port is not (yet) possible.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// let port: ExposePort = FromStr::from_str("80/tcp").unwrap();
+    /// assert_eq!(port.host_port, 80);
+    /// assert_eq!(port.container_port, None);
+    /// assert_eq!(port.family, "tcp");
+    /// ```
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let split: Vec<&str> = s.split('/').collect();
         Ok(match split.len() {
@@ -153,19 +426,84 @@ impl FromStr for ExposePort {
     }
 }
 
+/// The container-DNAT section, defining how containers can communicate with each other over
+/// non-common networks.
 #[derive(Deserialize, Debug, Clone)]
 #[serde(deny_unknown_fields)]
 pub struct ContainerDNAT {
+    /// An optional list of rules, see
+    /// [`ContainerDNATRule`](struct.ContainerDNATRule.html).
+    ///
+    /// # Example
+    ///
+    /// The easiest way to define the rules is using TOMLs [arrays of tables][toml-aot]:
+    ///
+    /// ```toml
+    /// [[container_dnat.rules]]
+    /// # first rule here
+    /// [[container_dnat.rules]]
+    /// # second rule here
+    /// ```
+    ///
+    /// [toml-aot]:
+    ///  https://github.com/toml-lang/toml/blob/master/versions/en/toml-v0.4.0.md#array-of-tables
     pub rules: Option<Vec<ContainerDNATRule>>,
 }
 
+/// Definition for a rule to be used in the container-DNAT section.
 #[derive(Deserialize, Debug, Clone)]
 #[serde(deny_unknown_fields)]
 pub struct ContainerDNATRule {
+    /// Network of the source container to apply the rule to.
     pub src_network: Option<String>,
+
+    /// Source container to apply the rule to.
     pub src_container: Option<String>,
+
+    /// Network of the destination container to apply the rule to.
     pub dst_network: String,
+
+    /// Destination container to apply the rule to.
     pub dst_container: String,
+
+    /// Ports to apply the rule to.
+    ///
+    /// Defined as:
+    ///
+    /// * a single integer
+    ///
+    /// * a single string
+    ///
+    /// * a single struct
+    ///
+    /// * a list of integers
+    ///
+    /// * a list of strings
+    ///
+    /// * a list of structs
+    ///
+    /// # Example
+    ///
+    /// All of the following are legal TOML fragments:
+    ///
+    /// ```toml
+    /// expose_port = 80
+    /// expose_port = [80, 443]
+    /// expose_port = "53/udp"
+    /// expose_port = ["80/tcp", "53/udp"]
+    ///
+    /// # The following four all result in the same definition
+    /// expose_port = { host_port = 8080 }
+    /// expose_port = { host_port = 8080, container_port = 8080 }
+    /// expose_port = { host_port = 8080, family = "tcp" }
+    /// expose_port = { host_port = 8080, container_port = 8080, family = "tcp" }
+    ///
+    /// expose_port = [
+    ///     { host_port = 80 },
+    ///     { host_port = 53, family = "udp" },
+    ///     { host_port = 443, container_port = 8443 },
+    /// ]
+    /// ```
     #[serde(deserialize_with = "single_or_seq_string_or_struct")]
     pub expose_port: Vec<ExposePort>,
 }
