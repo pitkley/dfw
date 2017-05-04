cbbe1758f44e7ec73a105e3b0f6bf2cf72247fec
Pit Kleyersburg <pitkley@googlemail.com>
2017-05-04 16:24:46 +0200

    Add helper to tests to support regex comparisons

--- tests/dfwrs.rs
+++ tests/dfwrs.rs
@@ -1,6 +1,8 @@
 #![cfg(feature = "docker-tests")]
 
 extern crate dfwrs;
+extern crate eval;
+extern crate regex;
 extern crate shiplift;
 #[macro_use]
 extern crate slog;
@@ -9,6 +11,7 @@ use dfwrs::*;
 use dfwrs::iptables::IPTablesLogger;
 use dfwrs::types::*;
 use dfwrs::util::load_file;
+use regex::Regex;
 use shiplift::Docker;
 use slog::{Drain, Fuse, Logger, OwnedKVList, Record};
 use std::fs::File;
@@ -22,18 +25,98 @@ use std::process::Command;
 static PROCESSING_OPTIONS: ProcessingOptions =
     ProcessingOptions { container_filter: ContainerFilter::Running };
 
-fn load_log(log_path: &str) -> Vec<(String, String)> {
-    let file = BufReader::new(File::open(log_path).unwrap());
+#[derive(Debug)]
+struct LogLine {
+    function: String,
+    regex: bool,
+    command: String,
+    eval: Option<String>,
+}
 
-    file.lines()
-        .into_iter()
-        .filter(Result::is_ok)
-        .map(Result::unwrap)
-        .map(|e| {
-                 let mut s = e.splitn(2, ' ');
-                 (s.next().unwrap().trim().to_owned(), s.next().unwrap().trim().to_owned())
-             })
-        .collect::<Vec<_>>()
+impl PartialEq for LogLine {
+    fn eq(&self, other: &LogLine) -> bool {
+        // If `function` is unequal, we don't have to do further comparisons
+        if self.function != other.function {
+            return false;
+        }
+
+        if self.regex {
+            if other.regex {
+                // Both are regex, not equal by our definition.
+                return false;
+            }
+
+            // Handle regex
+            let re = Regex::new(&self.command).unwrap();
+
+            // Verify we have a match
+            if !re.is_match(&other.command) {
+                return false;
+            }
+
+            // Check if we have to have constraints to evaluate
+            if let Some(ref eval) = self.eval {
+                // Get capture groups
+                let captures = re.captures(&other.command).unwrap();
+
+                // Try to expand the capture groups used in the eval-string
+                let mut expansion = String::new();
+                captures.expand(&eval, &mut expansion);
+
+                // Evaluate the string
+                let e = eval::eval(&expansion);
+                return e.is_ok() && e.unwrap() == eval::to_value(true);
+            } else {
+                // Nothing to evaluate, `is_match` was successful.
+                return true;
+            }
+        } else {
+            if other.regex {
+                // We don't want to duplicate the regex handling, just ask `other` for the result.
+                return other.eq(self);
+            } else {
+                // No regex involved, just `command` left to compare
+                return self.command == other.command;
+            }
+        }
+    }
+}
+
+impl Eq for LogLine {}
+
+fn load_log(log_path: &str) -> Vec<LogLine> {
+    let file = BufReader::new(File::open(log_path).unwrap());
+    let mut v = Vec::new();
+
+    for line in file.lines() {
+        if line.is_err() {
+            continue;
+        }
+        let line = line.unwrap();
+
+        let s = line.split("\t").collect::<Vec<_>>();
+        v.push(match s.len() {
+                   2 => {
+                       LogLine {
+                           function: s[0].to_owned(),
+                           command: s[1].to_owned(),
+                           regex: false,
+                           eval: None,
+                       }
+                   }
+                   3 => {
+                       LogLine {
+                           function: s[0].to_owned(),
+                           command: s[1].to_owned(),
+                           regex: true,
+                           eval: Some(s[2].to_owned()),
+                       }
+                   }
+                   _ => panic!("log line split incorrectly"),
+               });
+    }
+
+    v
 }
 
 fn logger() -> Logger {
@@ -146,13 +229,33 @@ fn dc_template(num: &str) {
         assert_eq!(result.unwrap(), ());
 
         // Verify logs for iptables (IPv4)
-        let logs4 = ipt4.logs();
+        let logs4 = ipt4.logs()
+            .iter()
+            .map(|&(ref f, ref c)| {
+                     LogLine {
+                         function: f.to_owned(),
+                         command: c.to_owned(),
+                         regex: false,
+                         eval: None,
+                     }
+                 })
+            .collect::<Vec<_>>();
         let expected4 =
             load_log(&resource(&format!("docker/{}/expected-iptables-v4-logs.txt", num)).unwrap());
         assert_eq!(logs4, expected4);
 
         // Verify logs for ip6tables (IPv6)
-        let logs6 = ipt6.logs();
+        let logs6 = ipt6.logs()
+            .iter()
+            .map(|&(ref f, ref c)| {
+                     LogLine {
+                         function: f.to_owned(),
+                         command: c.to_owned(),
+                         regex: false,
+                         eval: None,
+                     }
+                 })
+            .collect::<Vec<_>>();
         let expected6 =
             load_log(&resource(&format!("docker/{}/expected-iptables-v6-logs.txt", num)).unwrap());
         assert_eq!(logs6, expected6);
