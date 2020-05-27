87fa8ce601612480f41fc5490dd38412d5781aa8
Pit Kleyersburg <pitkley@googlemail.com>
2020-05-27 17:09:02 +0200

    Deploy documentation for 1.0.0 to GitHub pages

---
+++ 1.0.0/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["dfw"] = {"name":"","files":["errors.rs","lib.rs","nftables.rs","process.rs","rule.rs","types.rs","util.rs"]};
+createSourceSidebar();
