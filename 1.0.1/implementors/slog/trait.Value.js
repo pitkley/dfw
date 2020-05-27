0d9bd50bb8f6b442f2d5f482a2ee6ceab1d09325
Pit Kleyersburg <pitkley@googlemail.com>
2020-05-27 17:09:03 +0200

    Deploy documentation for 1.0.1 to GitHub pages

---
+++ 1.0.1/implementors/slog/trait.Value.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["dfw"] = [{"text":"impl Value for <a class=\"enum\" href=\"dfw/nftables/enum.ChainPolicy.html\" title=\"enum dfw::nftables::ChainPolicy\">ChainPolicy</a>","synthetic":false,"types":["dfw::nftables::ChainPolicy"]},{"text":"impl Value for <a class=\"enum\" href=\"dfw/nftables/enum.RuleVerdict.html\" title=\"enum dfw::nftables::RuleVerdict\">RuleVerdict</a>","synthetic":false,"types":["dfw::nftables::RuleVerdict"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
